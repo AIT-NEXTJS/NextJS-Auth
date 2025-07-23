dbc0673acbd5407d138fd7f35f92c6ed54053c8b
Romyleads <romyleads@gmail.com>
2025-07-23 11:12:17 +0200

    update NavBar and SportsLayout components to enhance styling and improve text visibility

--- src/app/sports/layout.tsx
+++ src/app/sports/layout.tsx
@@ -7,14 +7,14 @@ export default function SportsLayout({
 }>) {
 	return (
 		<>
-			<nav className='flex justify-center items-center gap-8 font-medium sticky top-[56px] z-10 bg-blue-950'>
-				<Link href='/sports/football' className='hover:text-blue-500'>
+			<nav className='flex justify-center items-center gap-8 font-medium sticky top-[56px] z-10 bg-blue-950 text-gray-100 py-3'>
+				<Link href='/sports/football' className='hover:text-orange-300 transition'>
 					Футбол
 				</Link>
-				<Link href='/sports/tennis' className='hover:text-blue-500'>
+				<Link href='/sports/tennis' className='hover:text-orange-300 transition'>
 					Теннис
 				</Link>
-				<Link href='/sports/swimming' className='hover:text-blue-500'>
+				<Link href='/sports/swimming' className='hover:text-orange-300 transition'>
 					Плавание
 				</Link>
 			</nav>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 export default function NavBar() {
 	return (
-		<nav className='flex justify-center items-center gap-6 min-h-12 sticky top-0 bg-slate-900'>
-			<Link href='/' className='hover:text-orange-300'>
+		<nav className='flex justify-center items-center gap-6 min-h-12 sticky top-0 bg-slate-900 text-gray-100 z-20'>
+			<Link href='/' className='hover:text-orange-300 transition'>
 				Главная
 			</Link>
-			<Link href='/about' className='hover:text-orange-300'>
+			<Link href='/about' className='hover:text-orange-300 transition'>
 				О проекте
 			</Link>
-			<Link href='/sports' className='hover:text-orange-300'>
+			<Link href='/sports' className='hover:text-orange-300 transition'>
 				Курсы по спорту
 			</Link>
 		</nav>
