bf15b591b281d59abb02547d3fa292da8e51738b
Romyleads <romyleads@gmail.com>
2025-07-25 09:29:32 +0200

    remove unused image remote patterns from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,66 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'assets.example.com',
-        pathname: '/account123/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.escuelajs.co',
-        pathname: '/api/v1/files/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.pixabay.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placeimg.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn1.gstatic.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.dexerto.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        pathname: '/**',
-      },
-{
-  protocol: 'https',
-  hostname: 'oldnavy.gap.com',
-  pathname: '/**',
-}
-
-    ],
-  },
+  /* config options here */
+  output: "export",
 };
 
 export default nextConfig;
