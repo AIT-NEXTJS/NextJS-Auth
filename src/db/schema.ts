2a71d68c275452a4aa265134078b7588d90d68fe
Romyleads <romyleads@gmail.com>
2025-07-28 19:16:08 +0200

    add lesson 17: integrate Drizzle ORM with PostgreSQL and create sports API
    - Added dotenv for environment variable management.
    - Introduced drizzle-orm for database interactions.
    - Configured drizzle with PostgreSQL in drizzle.config.ts.
    - Created sports API with GET and POST endpoints in route.ts.
    - Established database connection in index.ts.
    - Defined sports table schema in schema.ts.
    - Updated package.json to include necessary dependencies.

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+import { integer, varchar } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const sportsTable = pgTable("sports", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull().unique(),
+  image: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 255 }).notNull(),
+}); // Create the "sports" table
