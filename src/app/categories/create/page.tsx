c6eff78f148c07dc9d3770e39950bd59932e71eb
Romyleads <romyleads@gmail.com>
2025-07-28 09:03:12 +0200

    add server-based create product page

--- src/app/categories/create/page.tsx
+++ src/app/categories/create/page.tsx
@@ -3,7 +3,7 @@ import { createCategory } from '@/app/actions/createCategory';
 export default function CreateCategory() {
 	return (
 		<div className='max-w-xl mx-auto p-6 bg-white rounded-xl'>
-			<h1 className='text-2xl font-bold text-center mb-6'>Add New Category</h1>
+			<h1 className='text-2xl font-bold  mb-6'>Create Category</h1>
 			<form action={createCategory} className='space-y-4'>
 				<input type='text' name='name' placeholder='Category name' className='w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-200' />
 				<input type='text' name='image' placeholder='Image URL' className='w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-200' />
