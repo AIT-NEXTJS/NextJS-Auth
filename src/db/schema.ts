760c8ad15316af776b6acb1e100b97c39fdf7523
Romyleads <romyleads@gmail.com>
2025-07-30 07:18:24 +0200

    fix schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,15 +1,15 @@
-import { integer, varchar } from "drizzle-orm/pg-core";
+import { integer, serial, varchar } from "drizzle-orm/pg-core";
 import { pgTable } from "drizzle-orm/pg-core";
 
 export const sportsTable = pgTable("sports", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: serial("id").primaryKey(),
   title: varchar({ length: 255 }).notNull().unique(),
   image: varchar({ length: 255 }).notNull(),
   description: varchar({ length: 255 }).notNull(),
 }); // Create the "sports" table
 
 export const eventsTable = pgTable("events", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: serial("id").primaryKey(),
   name: varchar({ length: 255 }).notNull().unique(),
   description: varchar({ length: 255 }).notNull(),
 description2: varchar({ length: 255 }).default(''),
